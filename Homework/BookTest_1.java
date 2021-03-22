a05266ae17cf9be98ee8f168a1535530b7a0cdb9
moonbaaang <qudwjsdl0814@naver.com>
2021-03-22 23:07:50 +0900

    update homework_1

--- Homework/BookTest_1.java
+++ Homework/BookTest_1.java
@@ -21,13 +21,13 @@ class Book{
 }
 
 class Magazine{
-	private String isbn;
-	private String title;
-	private String author;
-	private String publisher;
-	private int price;
-	private int year;
-	private int month;
+	String isbn;
+	String title;
+	String author;
+	String publisher;
+	int price;
+	int year;
+	int month;
 	String desc;
 	
 	
@@ -40,54 +40,6 @@ class Magazine{
 		this.year = year;
 		this.month = month;
 		this.price = price;
-		this.desc = desc;
-	}
-	public String getIsbn() {
-		return isbn;
-	}
-	public void setIsbn(String isbn) {
-		this.isbn = isbn;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getAuthor() {
-		return author;
-	}
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-	public String getPublisher() {
-		return publisher;
-	}
-	public void setPublisher(String publisher) {
-		this.publisher = publisher;
-	}
-	public int getYear() {
-		return year;
-	}
-	public void setYear(int year) {
-		this.year = year;
-	}
-	public int getMonth() {
-		return month;
-	}
-	public void setMonth(int month) {
-		this.month = month;
-	}
-	public int getPrice() {
-		return price;
-	}
-	public void setPrice(int price) {
-		this.price = price;
-	}
-	public String getDesc() {
-		return desc;
-	}
-	public void setDesc(String desc) {
 		this.desc = desc;
 	}
 	@Override
